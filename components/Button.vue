68fb7926e23b9d55dc9caf44da641661c9a344ce
unknown <markwestgeest@gmail.com>
2020-09-07 17:36:51 +0200

    Added a button component to prevent repeating code in different files where PageButtonRecords are used

---
+++ components/Button.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+  <div>
+    <nuxt-link v-if="type == 'intern'" :to="url" :class="'btn btn-primary ' + sizeClass + ' ' + classList">
+      {{title}}
+    </nuxt-link>
+
+    <a v-else :href="url" :class="'btn btn-primary ' + sizeClass" target="_blank">
+      {{title}}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    url: {
+      type: String
+    },
+    size: {
+      type: String,
+      default: null
+    },
+    classList: {
+      type: String
+    }
+  },
+  data: function() {
+    return {
+      sizeClass: ''
+    }
+  },
+  mounted() {
+    // Set button size
+    if(this.size != undefined && this.size != null) {
+      // Check button size and give it a bootstrap class
+      switch(this.size) {
+        case 'small':
+          this.sizeClass = 'btn-sm'
+          break
+
+        case 'normal':
+          this.sizeClass = ''
+          break
+
+        case 'large':
+          this.sizeClass = 'btn-lg'
+          break
+
+        default:
+          this.sizeClass = ''
+
+      }
+    }
+  }
+}
+</script>
