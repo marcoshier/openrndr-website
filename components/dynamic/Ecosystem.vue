efdedf85120967a07f935b1bd5886b7cc5b99aac
unknown <markwestgeest@gmail.com>
2020-09-11 17:03:16 +0200

    Created a component for retrieving all Ecosystem items. Created a component for displaying Ecosystem items

--- components/dynamic/Ecosystem.vue
+++ components/dynamic/Ecosystem.vue
@@ -1,11 +1,45 @@
 <template lang="html">
-
+  <div v-if="allEcosystems" class="row row-eq-height px-5 pt-5">
+    <ecosystem-item v-for="(item, index) in allEcosystems"
+      :title="item.title"
+      :description="item.description"
+      :image="item.image"
+      :index="index"
+    />
+  </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import EcosystemItem from '~/components/dynamic/EcosystemItem.vue'
+
 export default {
+  apollo: {
+    allEcosystems: gql`{
+      allEcosystems {
+        title
+        description
+        image {
+          url
+          alt
+          title
+        }
+      }
+    }`
+  },
+  components: {
+    EcosystemItem
+  },
+  props: {
+    name: {
+      type: String
+    },
+    page: {
+      type: Object
+    }
+  }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 </style>
---
+++ components/dynamic/EcosystemItem.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+  <div class="col-12 col-lg-6 col-xl-4 mb-5 d-flex">
+    <div class="card w-100">
+      <a class="eco-link h-100" href="#">
+        <div class="card-body d-flex h-100 p-3 align-items-center row">
+          <div class="col-4 eco-img-wrapper text-center px-3">
+            <img class="eco-img img-fluid d-inline" :src="image.url">
+          </div>
+          <div class="col-8 pl-0">
+            <h4 class="eco-title mb-1">{{title}}</h4>
+            <p class="eco-description mb-0">{{description}}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    image: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.eco-link {
+  color: inherit;
+}
+
+.eco-link:hover{
+  text-decoration: none;
+  color: inherit;
+}
+
+.eco-img {
+}
+
+.eco-img-wrapper {
+  width: 65px;
+  overflow: hidden;
+}
+</style>
