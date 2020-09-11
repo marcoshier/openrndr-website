89d13ad46ba5b593afdbdbaf6ed2f310726be2e7
unknown <markwestgeest@gmail.com>
2020-09-11 16:40:14 +0200

    Created component for retrieving KeyFeatures collection. Created a component for displaying KeyFeatureItems

---
+++ components/dynamic/KeyFeatureItem.vue
@@ -0,0 +1,73 @@
+<template lang="html">
+  <div class="col-12 col-md-6 mb-5 d-flex px-0" v-bind:class="{'pr-3': isFirstInRow, 'pl-3': !isFirstInRow}">
+    <div class="feature-number mr-5">
+      <img class="img-fluid" src="@/assets/images/key-feature-circle.png">
+      <h1 class="mb-0 feature-number-text">{{index + 1}}</h1>
+    </div>
+
+    <div>
+      <h4>{{title}}</h4>
+      <p class="mb-0">{{description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    index: {
+      type: Number
+    }
+  },
+  data: function() {
+    return {
+      isFirstInRow: null
+    }
+  },
+  mounted() {
+    if(this.index != undefined) {
+      let index = this.index + 1
+
+      // If remainder is not 0, this means the component's index is uneven and first in the row
+      if(index % 2 != 0) {
+        this.isFirstInRow = true
+      } else {
+        this.isFirstInRow = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.feature-number {
+  position: relative;
+  min-width: 90px;
+  min-height: 90px;
+  height: fit-content;
+
+  & > img {
+    min-width: 90px;
+    min-height: 90px;
+  }
+
+  & > {
+    .feature-number-text {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+}
+
+
+
+</style>
--- components/dynamic/KeyFeatures.vue
+++ components/dynamic/KeyFeatures.vue
@@ -1,11 +1,36 @@
 <template lang="html">
-
+  <div v-if="allFeatures" class="row px-5 pt-5 m-0">
+    <key-feature-item v-for="(feature, index) in allFeatures"
+      :title="feature.title"
+      :description="feature.description"
+      :index="index"
+    />
+  </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import KeyFeatureItem from '~/components/dynamic/KeyFeatureItem.vue'
+
 export default {
+  apollo: {
+    allFeatures: gql`{
+      allFeatures {
+        title
+        description
+      }
+    }`
+  },
+  components: {
+    KeyFeatureItem
+  },
+  props: {
+    name: {
+      type: String
+    },
+    page: {
+      type: Object
+    }
+  }
 }
 </script>
-
-<style lang="css" scoped>
-</style>
