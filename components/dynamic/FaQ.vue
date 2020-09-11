65938705085eaf38307f3b53132425612fab2d44
unknown <markwestgeest@gmail.com>
2020-09-11 17:51:17 +0200

    Created a component to retrieve and display all Faqs. Edited styling a bit so that clickable title is underlined with primary color on hover and when collapsed.

--- components/dynamic/FaQ.vue
+++ components/dynamic/FaQ.vue
@@ -1,11 +1,47 @@
 <template lang="html">
-
+  <div v-if="allFaqs" role="tablist" class="px-5 pt-5 faq">
+    <div v-for="(question, index) in allFaqs" :key="index" class="mb-5">
+      <div role="tab">
+        <a class="question" v-b-toggle :href="'#accordion-'+index"><h4 class="mb-2">{{question.question}}</h4></a>
+      </div>
+      <b-collapse :id="'accordion-'+index" accordion="faq">
+        <div v-html="question.answer">
+        </div>
+      </b-collapse>
+    </div>
+  </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
+  apollo: {
+    allFaqs: gql`{
+      allFaqs {
+        question
+        answer
+      }
+    }`
+  },
+  props: {
+    name: {
+      type: String
+    },
+    page: {
+      type: Object
+    }
+  }
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+@import '@/assets/css/variables.scss';
+
+ .faq {
+    .question:hover, a.not-collapsed {
+     color: $primary !important;
+     text-decoration: underline;
+   }
+ }
 </style>
