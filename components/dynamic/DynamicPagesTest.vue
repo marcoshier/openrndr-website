ada0f036474f55d0c1fb8765b4284c70d2a5ba4c
unknown <>
2021-01-07 13:11:09 +0100

    Created a test component to test out a modular page component. This must eventually replace the repeated content of index.vue in pages/, pages/_slug and pages/_slug/_sublsug.

---
+++ components/dynamic/DynamicPagesTest.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    allPages: gql`{
+      allPages {
+        slug
+      }
+    }`
+  },
+  mounted() {
+    foreach(this.allPages)
+    console.log(this.allPages)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
