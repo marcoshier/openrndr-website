aeb3ebec55e631f1ba5b54aaace4bdbb7c7cafa5
Mark Westgeest <markwestgeest@gmail.com>
2021-03-27 21:07:17 +0100

    Retrieving Pages and Calendar items now inside vuex state at nuxtServerInit. Created getters for currentPage and currentChildPage based on slug.

--- store/index.js
+++ store/index.js
@@ -3,22 +3,42 @@ import getFeaturesQuery from "~/apollo/queries/landing/keyFeatures.gql"
 import getEcosystemsQuery from "~/apollo/queries/landing/ecosystems.gql"
 import getFaqsQuery from "~/apollo/queries/landing/faqs.gql"
 
+import getAllPagesQuery from "~/apollo/queries/pages/allPages.gql"
+import getAllPageChildren from "~/apollo/queries/pages/allPageChildren.gql"
+
+import getAllCalendars from "~/apollo/queries/dynamic/allCalendars.gql"
+
 export const state = () => ({
   landingPage: {
     content: {},
     allKeyFeatures: {},
     allEcosystems: {},
     allFaqs: {}
+  },
+  pages: {
+    allPages: [],
+    allPageChildren: [],
+    current: {}
+  },
+  dynamic: {
+    allCalendars: [],
   }
 })
 
 export const getters = {
   landingPage: state => {
     return state.landing
+  },
+  currentPage: (state) => (slug) => {
+    return state.pages.allPages.find(page => page.slug === slug)
+  },
+  currentPageChild: (state) => (slug) => {
+    return state.pages.allPageChildren.find(page => page.slug === slug)
   }
 }
 
 export const mutations = {
+  // Landing
   set_landing_page: function(state, content) {
     state.landingPage.content = content
   },
@@ -31,6 +51,19 @@ export const mutations = {
   set_landing_faqs: function(state, faqs) {
     state.landingPage.allFaqs = faqs
   },
+
+  // Pages
+  set_pages_all: function(state, pages) {
+    state.pages.allPages = pages
+  },
+  set_pages_children: function(state, pages) {
+    state.pages.allPageChildren = pages
+  },
+
+  // Dynamic
+  set_calendars: function(state, items) {
+    state.dynamic.allCalendars = items
+  }
 }
 
 
@@ -38,6 +71,8 @@ export const actions = {
   nuxtServerInit({ commit }, { error }) {
    const clientApollo = this.app.apolloProvider.defaultClient
    const obj = {}
+
+   // Landing
    obj.landing =  new Promise((resolve, reject) => {
      clientApollo
        .query({
@@ -51,7 +86,6 @@ export const actions = {
          resolve(err)
        })
    })
-
    obj.allFeatures =  new Promise((resolve, reject) => {
      clientApollo
        .query({
@@ -65,7 +99,6 @@ export const actions = {
          resolve(err)
        })
    })
-
    obj.allEcosystems =  new Promise((resolve, reject) => {
      clientApollo
        .query({
@@ -79,7 +112,6 @@ export const actions = {
          resolve(err)
        })
    })
-
    obj.allFaqs =  new Promise((resolve, reject) => {
      clientApollo
        .query({
@@ -87,6 +119,48 @@ export const actions = {
        })
        .then(resp => {
          commit("set_landing_faqs", resp.data.allFaqs)
+         resolve(resp)
+       })
+       .catch(err => {
+         resolve(err)
+       })
+   })
+
+   // Pages
+   obj.allPages =  new Promise((resolve, reject) => {
+     clientApollo
+       .query({
+         query: getAllPagesQuery
+       })
+       .then(resp => {
+         commit("set_pages_all", resp.data.allPages)
+         resolve(resp)
+       })
+       .catch(err => {
+         resolve(err)
+       })
+   })
+   obj.allPageChildren =  new Promise((resolve, reject) => {
+     clientApollo
+       .query({
+         query: getAllPageChildren
+       })
+       .then(resp => {
+         commit("set_pages_children", resp.data.allPageChildren)
+         resolve(resp)
+       })
+       .catch(err => {
+         resolve(err)
+       })
+   })
+
+   obj.allCalendars =  new Promise((resolve, reject) => {
+     clientApollo
+       .query({
+         query: getAllCalendars
+       })
+       .then(resp => {
+         commit("set_calendars", resp.data.allCalendars)
          resolve(resp)
        })
        .catch(err => {
