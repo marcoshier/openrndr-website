32ef38f5a2c1d9708a871672c2e751f36bc52185
shierone <dellabatemarco@gmail.com>
2021-08-06 17:23:41 +0200

    Added ecosystem links

--- components/dynamic/EcosystemItem.vue
+++ components/dynamic/EcosystemItem.vue
@@ -1,6 +1,7 @@
 <template lang="html">
    <div class="mb-1 mb-lg-1 px-0 px-lg-3">
-    <div class="card border-0">
+    
+    <div class="card border-0" :class="{'hovered' : hovered}" v-on:mouseover="showLinks" v-on:mouseout="hideLinks">
       <a v-if="url" class="eco-link h-100" :href="url">
         <div class="card-body d-flex h-100 p-3 align-items-center mb-4 mb-sm-0">
           <div class="col-12 eco-img-wrapper text-center px-3">
@@ -15,6 +16,15 @@
           </div>
         </div>
       </div>
+    </div>
+    <div class="links-modal p-3">
+      <a v-if="url" :href="url">
+         Example
+      </a>
+      <span> ‎| ‎</span>
+      <a v-if="url" :href="url">
+         Docs
+      </a>
     </div>
   </div>
 </template>
@@ -34,25 +44,47 @@ export default {
     },
     image: {
       type: Object
+    },
+  },
+  data: function(){
+    return {
+      hovered: false
+    }
+  },
+  methods: {
+    showLinks() {
+      this.hovered = true
+    },
+    hideLinks() {
+      this.hovered = false
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.links-modal {
+  position: relative;
+  background: #ffc0cb;
+  text-align: center;
+  border-radius: 2px;
+  bottom: 20px;
+}
+
 .card {
   display: inline-block;
 }
 
 .card {
-  transform: scale(1);
+  left: 50%;
+  transform: translateX(-50%);
   transition: all 0.1s ease;
   background-color: transparent !important;
   height: 120px
 }
 
 .card:hover {
-  transform: scale(0.97);
+  transform: translateX(-50%);
   transition: all 0.2s ease;
 }
 
