c9abee4e2c99525926b67b215bd01f4b3ca7e6a0
shierone <dellabatemarco@gmail.com>
2021-09-22 15:32:42 +0200

    added gAnalytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,19 @@
 // import dynamicRoutes from '~plugins/dynamicRoutes.js'
 import 'cross-fetch/polyfill'
 import { createHttpLink } from "apollo-link-http"
-import {execute, makePromise} from 'apollo-link'
+import { execute, makePromise } from 'apollo-link'
 import gql from 'graphql-tag'
 
 export default {
-  mode: 'universal',
-  target: 'static', // default is 'server'
-  generate: {
-    routes: async (callback) => {
-      // 1. Get the list of posts
-      const uri = 'https://graphql.datocms.com'
-      const link = new createHttpLink({ uri: uri, fetch: fetch })
-      const operation = {
-          query: gql`
+    mode: 'universal',
+    target: 'static', // default is 'server'
+    generate: {
+        routes: async(callback) => {
+            // 1. Get the list of posts
+            const uri = 'https://graphql.datocms.com'
+            const link = new createHttpLink({ uri: uri, fetch: fetch })
+            const operation = {
+                query: gql `
           {
               allPages {
                   slug
@@ -25,121 +25,128 @@ export default {
                 slug
               }
           }`,
-          context: {
-              headers: {
-                  authorization: 'a062c88e12d211da4cc3c596601b03'
-              }
-          }
-      }
-      let data = await makePromise(execute(link, operation))
+                context: {
+                    headers: {
+                        authorization: 'a062c88e12d211da4cc3c596601b03'
+                    }
+                }
+            }
+            let data = await makePromise(execute(link, operation))
 
-      // 2. Build the list of routes for Nuxt
-      var pageRoutes = [];
+            // 2. Build the list of routes for Nuxt
+            var pageRoutes = [];
 
-      data.data.allPages.forEach(function(item) {
-          pageRoutes.push('/' + item.slug)
-      })
+            data.data.allPages.forEach(function(item) {
+                pageRoutes.push('/' + item.slug)
+            })
 
-      data.data.allPageChildren.forEach(function(item) {
-          pageRoutes.push('/' + item.parentPage.slug + '/' + item.slug)
-      })
+            data.data.allPageChildren.forEach(function(item) {
+                pageRoutes.push('/' + item.parentPage.slug + '/' + item.slug)
+            })
 
-      // Register the routes
-      callback(null, pageRoutes)
+            // Register the routes
+            callback(null, pageRoutes)
+        },
+        exclude: [
+            '/index (old)'
+        ]
     },
-    exclude: [
-      '/index (old)'
-    ]
-  },
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: false,
-  /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/css/main.scss'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~plugins/vue-gallery.client.js',
-    '~plugins/vue-progress.js'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxtjs/dotenv',
-    '@nuxtjs/fontawesome'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/apollo',
-  ],
-  fontawesome: {
-    icons: {
-      solid: [
-        'faBars',
-        'faTimes',
-        'faChevronDown',
-        'faChevronUp',
-        'faCalendarTimes',
-        'faCalendarDay',
-        'faCalendarAlt',
-        'faMapMarkerAlt',
-        'faPhoneAlt',
-        'faEnvelope'
-      ],
-      regular: [
-        'faClock'
-      ],
-      brands: [
-        'faInstagram'
-      ]
-    }
-  },
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  apollo: {
-    clientConfigs: {
-      default: '~/apollo/config.js'
-    }
-  },
-  axios: {
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: false,
+    /*
+     ** Global CSS
+     */
+    css: [
+        '@/assets/css/main.scss'
+    ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        '~plugins/vue-gallery.client.js',
+        '~plugins/vue-progress.js'
+    ],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [
+        '@nuxtjs/dotenv',
+        '@nuxtjs/fontawesome',
+        '@nuxtjs/google-analytics'
+    ],
+    googleAnalytics: {
+        id: 'UA-109731993-2' || process.env.GOOGLE_ANALYTICS_ID
+    },
+    publicRuntimeConfig: {
+        googleAnalytics: {
+            id: process.env.GOOGLE_ANALYTICS_ID
+        }
+    },
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://bootstrap-vue.js.org
+        'bootstrap-vue/nuxt',
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/apollo',
+    ],
+    fontawesome: {
+        icons: {
+            solid: [
+                'faBars',
+                'faTimes',
+                'faChevronDown',
+                'faChevronUp',
+                'faCalendarTimes',
+                'faCalendarDay',
+                'faCalendarAlt',
+                'faMapMarkerAlt',
+                'faPhoneAlt',
+                'faEnvelope'
+            ],
+            regular: [
+                'faClock'
+            ],
+            brands: [
+                'faInstagram'
+            ]
+        }
+    },
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    apollo: {
+        clientConfigs: {
+            default: '~/apollo/config.js'
+        }
+    },
+    axios: {},
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
